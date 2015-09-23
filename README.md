add1963d265533142294627ef361eccacc00ac5f
Lorenzo Affetti <affox90@gmail.com>
2015-09-23 16:18:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Virtual Machine
+Are you on a different computer than yours and you want to get your terminal environment?
+
+* Get a provisioner ([Virtualbox](https://www.virtualbox.org/wiki/Downloads))
+* [Get Vagrant](https://www.vagrantup.com/)
+* Get the code at [https://github.com/affo/vim](https://github.com/affo/vim)
+
+Then, in `vmine`'s directory:
+
+```
+$ vagrant up # wait for the VM to be up
+$ vagrant ssh
+```
+
+Or something like this (I'm thinking of you, Windows).
---
+++ up.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Prerequisites
+sudo apt-get update && sudo apt-get -yq install git vim
+
+# For colors and autojump
+mkdir -p $HOME/.config
+git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
+git clone https://github.com/wting/autojump.git $HOME/autojump
+
+# Clone my repos
+git clone https://github.com/affo/dotfiles.git
+git clone https://github.com/affo/vim.git
+
+# Move dotfiles in homedir
+for df in `ls dotfiles`; do
+    cp dotfiles/$df ./.$df
+done
+
+echo ""
+echo ""
+echo "[vim] If you need vim configuration: \`\$ cd vim; make\`"
+echo ""
+echo "[autojump] For autojump settings: \`\$ cd autojump; make; source .bashrc\`"
+echo ""
